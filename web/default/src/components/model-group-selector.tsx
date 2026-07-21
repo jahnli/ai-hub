164150c1c4e8916a1b5db5331dfcb0cddb7ff05d
lijiaheng <lijiaheng@semi-tech.com>
2026-07-21 10:37:23 +0800

    fix(playground): prevent model group option stretching

--- web/default/src/components/model-group-selector.tsx
+++ web/default/src/components/model-group-selector.tsx
@@ -47,7 +47,7 @@ import { cn } from '@/lib/utils'
 import {
   modelGroupSelectorLayoutClasses,
   scrollSelectedOptionIntoView,
-} from './model-group-selector-layout'
+} from './model-group-selector/layout'
 
 interface ModelOption {
   label: string
